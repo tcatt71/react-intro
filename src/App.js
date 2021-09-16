54c1c416c18c390191c32e5ee6e91c7ea50771de
Troy Coffindaffer <troy.coffindaffer@gmail.com>
2021-09-16 15:45:23 -0700

    Add additional text to page

--- src/App.js
+++ src/App.js
@@ -5,9 +5,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>This is my first React App!</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. This is great!
         </p>
         <a
           className="App-link"
